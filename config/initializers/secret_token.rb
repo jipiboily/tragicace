5df77c495ae32d49c678c3c9de9b12e0305a7511
Jean-Philippe Boily <jipiboily@gmail.com>
2011-10-28 15:44:03 -0400

    Initial commit (only did 'rails new tragicace')

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tragicace::Application.config.secret_token = 'eb60b72a1d114d65000fec2224b66f84ed641daef0a0bb7fb0e540675ae0bbc317387bcd59bc66815ed11c92828ca0e7bb0a30f73b56ce87ec1188bd2c968378'
